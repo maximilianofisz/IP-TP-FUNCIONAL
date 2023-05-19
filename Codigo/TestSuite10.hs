4fc37d9c1dc4047461175ba34683efbf2768391f
mfisz <maxifisz@gmail.com>
2023-05-19 16:24:49 -0300

    aaa

---
+++ Codigo/TestSuite10.hs
@@ -0,0 +1,20 @@
+module TestSuite1 where
+import Test.HUnit
+import Solucion
+
+--nombresDeUsuarios :: RedSocial -> [String]
+--nombresDeUsuarios (x,y,z) = iterarUsuarios (x)
+
+--iterarUsuarios :: [Usuario] -> [String]
+--iterarUsuarios (x:[]) = [nombreDeUsuario x]
+--iterarUsuarios (x:xs) = [nombreDeUsuario x] ++ iterarUsuarios xs
+-- Datos para test suite 1
+
+ts1_redsocial1 = ([],[],[])
+ts1_redsocial2 = ([(1,"Maximiliano")],[],[])
+ts1_redsocial3 = ([(1,"Maximiliano"), (2,"Mauricio"),(3,"Lujan")],[],[])
+
+testSuite1 = test [
+    " nombresDeUsuarios 1" ~: (nombresDeUsuarios ts1_redsocial1) ~?= [],
+    " nombresDeUsuarios 2" ~: (nombresDeUsuarios ts1_redsocial2) ~?= ["Maximiliano"],
+    " nombresDeUsuarios 3" ~: (nombresDeUsuarios ts1_redsocial3) ~?= ["Maximiliano", "Mauricio", "Lujan"]
